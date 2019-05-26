ddaa4db80c470c1c8c18aeb9407ba2190038ab26
Syed Shakeeb <49868935+firas98@users.noreply.github.com>
2019-05-26 20:30:53 +0530

    Add files via upload

---
+++ Destini/Destini/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Destini
+//
+//  Created by Syed Shakeeb on 2019-05-26.
+//  Copyright © 2019 Syed Shakeeb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
